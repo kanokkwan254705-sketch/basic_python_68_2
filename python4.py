9833f2084fae4db483b0d8bf0a7b9ef210778dc6
kanokkwan <kanokkwan254705@gmail.com>
2025-11-22 14:46:33 +0700

    Condtional Statements

---
+++ python4.py
@@ -0,0 +1,19 @@
+"""
+#
+# Part : Condtional Statements
+#
+"""
+x = 10
+y = 20
+if x < y:
+    print("x isless than y")
+    print(1)
+elif x > y:
+    print("x greater than y")
+    print(2)
+elif x == y :
+    print("x equal to y")
+    print(3)
+else:
+    print("error")
+    print(4)
