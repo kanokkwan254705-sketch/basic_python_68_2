3184eab2a85127a8c9c858b6605b4b30023fa5e3
kanokkwan <kanokkwan254705@gmail.com>
2025-11-22 14:35:49 +0700

    Collection of Python

---
+++ python2.py
@@ -0,0 +1,52 @@
+"""
+#
+# Part : Python Operators
+# Arithmetic Operators
+#
+"""
+
+x = 4 + 5
+y= 10 - 2
+z= 3 * 2 
+a =8 / 2
+b = 10 % 3
+
+"""
+#
+# Part : Python Operators
+# Assigment Operators
+#
+"""
+
+x = 5 
+x = x + 3
+x += 3
+
+"""
+#
+# Part : Python Operators
+# Comparison Operators
+#
+"""
+x = 5
+y = 10
+print(x == y) #Fals
+print(x != y) #True
+print(x > y) #Fals
+print(x < y) #True
+print(x >= y) #Fals
+print(x <= y) #True
+
+"""
+#
+# Part : Python Operators
+# Logical Operators
+#
+"""
+x = 5
+y = 10
+print((x < 10) and (y > 5)) # True
+print((x < 10) or (y < 5)) # True
+print(not(x < 10))         #Fals
+print((x < 10) & (y > 5)) # True
+print((x < 10) | (y > 5)) # True
---
+++ python3.py
@@ -0,0 +1,41 @@
+"""
+#
+# Part : Collection of Python 
+# List, Tuples, Set, Dictionaries
+#
+"""
+"""
+#
+# List  
+# 
+"""
+# this_list = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(this_list)
+# print(this_list[1])
+# print(type(this_list))
+# print(len(this_list))
+
+"""
+#
+# Set 
+# 
+"""
+this_set = ["apple", "banana", "cherry", "apple", "cherry"]
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionaries
+# 
+"""
+this_dict = {
+     "name": "Jhon",
+     "age": 36,
+     "country": "Norway"
+}
+print(this_dict)
+print(this_dict["name"])
+print(type(this_dict))
+print(len(this_dict))
