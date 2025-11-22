437729246d922a0d7adfdb9388804ef3dd965468
kanokkwan <kanokkwan254705@gmail.com>
2025-11-22 15:14:09 +0700

    Functions

---
+++ python6.py
@@ -0,0 +1,21 @@
+"""
+#
+# Part: Functions 
+#
+"""
+def myFullname(firsName="Unknown", lastName="Forger"):
+    fullName = firsName + "" + lastName
+    return fullName
+
+print(myFullname("Loid", "Forger"))
+print(myFullname("Yor", "Forger"))
+print(myFullname("Anya", "Forger"))
+print(myFullname("Bond", "Forger"))
+
+def redpotion(hp):
+    hp+= 50
+    return hu
+currentHp = 100
+print("Current HP:", currentHp)
+CurrentHp = redPotion(urrentHp)
+print("After using RedPotion, HP:" currentHp)
