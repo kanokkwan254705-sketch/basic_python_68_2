06f97a4ac75be7ba0e889f2fdac925e500b36c84
kanokkwan <kanokkwan254705@gmail.com>
2025-11-22 13:30:22 +0700

    Variable

---
+++ python1.py
@@ -0,0 +1,39 @@
+"""
+#
+# Part: python comment
+#
+
+"""
+
+print("Hello, World!")
+
+"""
+#
+# Part: python Variable
+#
+
+"""
+x = 5
+y = "5"
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variable Naming
+#
+
+"""
+myvar = "John"
+my_var = "John" 
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+MY_VAR = "John"
+my_var2 = "John"
+
+
