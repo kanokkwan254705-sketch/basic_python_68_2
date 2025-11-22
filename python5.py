a5ce3a141b3eb6f1f106f8ce71e2efab22507c50
kanokkwan <kanokkwan254705@gmail.com>
2025-11-22 14:59:14 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,25 @@
+"""
+#
+# Part : Loop
+# Section: while Loop
+#
+"""
+
+i = 1
+while 1 <= 5:
+    print("Hello python", i)
+    if i ==3:
+        break
+    i += 1
+
+"""
+#
+# Part : Loop
+# Section: For Loop
+#
+"""
+this_list = ["apple", "banana", "cherry"]
+for x in this_list:
+    print(x)
+    if x =="banana":
+        break
